05ab8a8e1685dc41dea9d07fbf0e7f5d377ef722
sw-yx <swyx@dontemail.me>
2019-04-29 11:45:38 -0400

    lougout as people

--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -4,6 +4,9 @@ import useLoading from "../useLoading"
 
 export function Logout() {
   const identity = useIdentityContext()
+  const name =
+    (identity && identity.user && identity.user.user_metadata && identity.user.user_metadata.full_name) || "NoName"
+  
   const [isLoading, load] = useLoading()
   const logout = () => load(identity.logoutUser())
   return (
@@ -14,7 +17,7 @@ export function Logout() {
       <form className="form ">
         <p className="infoText">
           Logged in as <br />
-          <span className="infoTextEmail">Shawn Wang</span>
+          <span className="infoTextEmail">{name}</span>
         </p>
         <button type="submit" className={isLoading ? "btn saving" : "btn"} onClick={logout}>
           Log out
